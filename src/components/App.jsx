7b14908eb945a036036d58d5f8fb881903c53c88
Ivan-Prystay <prystay.ivan@gmail.com>
2022-12-18 16:30:03 +0200

    contacts.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import { contacts } from '../path/contacts';
+import contacts from '../path/contacts.json';
 
 export class App extends Component {
   state = {
